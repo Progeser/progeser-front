7839956183fb2c090943ad11ec7c1d6836e82c6d
Christopher Anciaux <christopher.anciaux@gmail.com>
2019-12-12 14:54:17 +0100

    feat - account requests list

--- src/app/controllers/grower/accounts-list/accounts-list.component.ts
+++ src/app/controllers/grower/accounts-list/accounts-list.component.ts
@@ -1,34 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {User} from '../../../models/user';
 import {AccountRequest} from '../../../models/account-request';
 import {AccountRequestService, SnackbarService} from '../../../services';
 import {PaginatedResource} from '../../../models/paginated-resource';
+import {TablePaginatorComponent} from '../../common/table-paginator/table-paginator.component';
+import {startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-accounts-list',
   templateUrl: './accounts-list.component.html',
   styleUrls: ['./accounts-list.component.scss']
 })
-export class AccountsListComponent implements OnInit {
+export class AccountsListComponent implements AfterViewInit {
   getRoleLabel = User.getRoleLabel;
 
   usersColumns: string[] = ['firstName', 'lastName', 'role', 'actions'];
-  users: User[] = [];
+  users: User[] = User.exampleData;
 
   accountRequestsColumns: string[] = ['firstName', 'lastName', 'actions'];
-  accountRequests: PaginatedResource<AccountRequest> = null;
+  accountRequests?: PaginatedResource<AccountRequest> = null;
 
-  numberItemsPerPage = 10;
+  @ViewChild('accountRequestsPaginator', {static: true})
+  accountRequestsPaginator: TablePaginatorComponent;
 
   constructor(protected snackbarService: SnackbarService,
-              protected httpAccountRequestService: AccountRequestService) { }
+              protected httpAccountRequestService: AccountRequestService) {
+  }
 
-  ngOnInit() {
-    this.users = User.exampleData;
-
-    // todo: paginator + table management
-    this.httpAccountRequestService.find().subscribe({
-      next: accountRequests => this.accountRequests = accountRequests,
+  ngAfterViewInit() {
+    this.accountRequestsPaginator.pageChange.pipe(
+      startWith({}),
+      switchMap(() => this.httpAccountRequestService.find(
+        this.accountRequestsPaginator.paginator.pageIndex + 1,
+        this.accountRequestsPaginator.paginator.pageSize
+      ))
+    ).subscribe({
+      next: accountRequests => this.accountRequests = accountRequests
     });
   }
 

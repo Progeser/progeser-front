c26710b9032e5e2575652d235cf2e3e12875e3ef
Christopher Anciaux <christopher.anciaux@gmail.com>
2020-01-18 11:16:36 +0100

    refactor - changed comparison method for shape form

--- src/app/components/shape-form/shape-form.component.ts
+++ src/app/components/shape-form/shape-form.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Shape, ModelableInterface} from '../../models/shape';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Shape} from '../../models/shape';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {ShapeService} from '../../services/http/shape/shape.service';
+import {compareByProperty} from '../../utils/comparators/compare-by-property';
 
 @Component({
   selector: 'app-shape-form',
@@ -8,18 +10,26 @@ import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./shape-form.component.scss']
 })
 export class ShapeFormComponent implements OnInit {
-  shapes: Shape[] = Shape.exampleData;
+  compareByName = compareByProperty('name');
+  shapes: Shape[];
 
   @Input()
   form: FormGroup;
 
-  constructor(protected formBuilder: FormBuilder) { }
+  constructor(protected formBuilder: FormBuilder,
+              protected httpShapeService: ShapeService) { }
 
   ngOnInit() {
+    console.log(this.form.get('shape').value);
+
+    this.httpShapeService.find().subscribe({
+      next: shapes => this.shapes = shapes
+    });
+
     this.form.get('shape').valueChanges.subscribe({
       next: (shape: Shape) => {
-        const temporaryDimensionsFormArrayControls = new Array(shape.numberDimensions)
-          .fill(null, 0, shape.numberDimensions)
+        const temporaryDimensionsFormArrayControls = new Array(shape.dimensionNames.length)
+          .fill(null, 0, shape.dimensionNames.length)
           .map(() => this.formBuilder.control(null));
 
         (this.form.get('dimensions') as FormArray).controls = temporaryDimensionsFormArrayControls;

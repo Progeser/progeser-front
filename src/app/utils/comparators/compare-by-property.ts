1c4544f0bcd69780504e87984306c695d5e6d949
Christopher Anciaux <christopher.anciaux@gmail.com>
2020-01-25 15:42:34 +0100

    fix - mat select compared objects can be null

--- src/app/utils/comparators/compare-by-property.ts
+++ src/app/utils/comparators/compare-by-property.ts
@@ -1,3 +1,8 @@
+import {isNullOrUndefined} from 'util';
+
 export function compareByProperty(propertyName) {
-  return (object1: any, object2: any) => object1[propertyName] === object2[propertyName];
+  return (object1: any, object2: any) =>
+    !isNullOrUndefined(object1)
+    && !isNullOrUndefined(object2)
+    && object1[propertyName] === object2[propertyName];
 }
